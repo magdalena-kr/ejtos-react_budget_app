d63a2adfae08d2bc6a2ab37534458751826c02f1
magdalena-kr <105656808+magdalena-kr@users.noreply.github.com>
2024-02-23 18:34:30 +0100

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -22,18 +22,64 @@ const ExpenseItem = (props) => {
             type: 'ADD_EXPENSE',
             payload: expense
         });
-
     }
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
+
+    const buttonStyle = {
+        borderRadius: '50%',
+        width: '30px',
+        height: '30px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        cursor: 'pointer',
+        marginRight: '5px', // Optional: Abstand zwischen den Buttons
+    };
+
+    const plusButtonStyle = {
+        ...buttonStyle,
+        backgroundColor: 'rgb(51, 204, 51)',
+        color: 'white',
+    };
+
+    const minusButtonStyle = {
+        ...buttonStyle,
+        backgroundColor: 'rgb(204, 0, 0)',
+        color: 'white',
+    };
 
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>€{props.cost}</td>
+            <td>
+                <button style={plusButtonStyle} onClick={event => increaseAllocation(props.name)}>
+                    +
+                </button>
+            </td>
+            <td>
+                <button style={minusButtonStyle} onClick={event => decreaseAllocation(props.name)}>
+                    -
+                </button>
+            </td>
+            <td>
+                <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
+            </td>
         </tr>
     );
 };
 
 export default ExpenseItem;
+
 
