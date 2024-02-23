144e1848493c109ba1eeab35050e284fe411f41f
magdalena-kr <105656808+magdalena-kr@users.noreply.github.com>
2024-02-23 16:59:44 +0100

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,22 +1,57 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Currency = () => {
     const { expenses, budget } = useContext(AppContext);
+    const [selectedCurrency, setSelectedCurrency] = useState(""); // Zustand für die ausgewählte Währung
+
+
     const totalExpenses = expenses.reduce((total, item) => {
         return (total = total + item.cost);
     }, 0);
     const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
+    const getCurrencySymbol = () => {
+        switch (selectedCurrency) {
+            case "Dollar":
+                return "$";
+            case "Pound":
+                return "£";
+            case "Euro":
+                return "€";
+            case "Ruppee":
+                return "₹";
+            default:
+                return "";
+        }
+    };
+
     return (
 
         
-        <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
-                        <option defaultValue>Currency...</option>
-                       
-                <option value="Dollar" name="finance">$ Dollar</option>
-                <option value="Pound" name="hr">£ Pound</option>
-                <option value="Euro" name="it">€ Euro</option>
-                <option value="Ruppee" name="admin">₹ Ruppee</option>
-                  </select>
+        <div>
+        <select
+            className="custom-select"
+            id="inputGroupSelect01"
+            onChange={(event) => setSelectedCurrency(event.target.value)}
+        >
+            <option defaultValue>Currency...</option>
+            <option value="Dollar" name="finance">
+                $ Dollar
+            </option>
+            <option value="Pound" name="hr">
+                £ Pound
+            </option>
+            <option value="Euro" name="it">
+                € Euro
+            </option>
+            <option value="Ruppee" name="admin">
+                ₹ Ruppee
+            </option>
+        </select>
+
+        <p>Selected Currency: {selectedCurrency}</p>
+        <p>Symbol: {getCurrencySymbol()}</p>
+    </div>
 
 
     );
